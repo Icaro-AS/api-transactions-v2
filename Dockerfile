098aeecb499e549796b1320902b68f9439ab4448
Ícaro Alencar <ias_epf@hotmail.com>
2025-05-14 02:05:16 -0300

    Criação dos arquivos docker e validação de alguns dtos

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+
+FROM node:23.11-alpine
+
+
+WORKDIR /app
+
+
+COPY package.json yarn.lock ./
+
+
+RUN yarn install --frozen-lockfile
+
+
+COPY . .
+
+RUN yarn build
+
+
+EXPOSE 3000
+
+
+CMD ["node", "dist/main"]
--- src/dtos/statistics.dto.ts
+++ src/dtos/statistics.dto.ts
@@ -1,7 +1,23 @@
+import { IsPositive, IsNumber } from 'class-validator';
+
 export class StatisticsDto {
+  @IsNumber()
+  @IsPositive()
   count: number;
+
+  @IsNumber()
+  @IsPositive()
   sum: number;
+
+  @IsNumber()
+  @IsPositive()
   avg: number;
+
+  @IsNumber()
+  @IsPositive()
   min: number;
+
+  @IsNumber()
+  @IsPositive()
   max: number;
 }
